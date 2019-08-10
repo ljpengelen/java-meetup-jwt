d6ba1e37a16d4a87b7725f6cd81d9e489f0ac176
Luc Engelen <luc@kabisa.nl>
2019-08-10 13:17:17 +0200

    Refactor authentication interceptor

--- back-end-jwt/src/main/java/nl/kabisa/meetup/jwtbased/interceptors/authentication/AuthenticationInterceptor.java
+++ back-end-jwt/src/main/java/nl/kabisa/meetup/jwtbased/interceptors/authentication/AuthenticationInterceptor.java
@@ -1,6 +1,5 @@
 package nl.kabisa.meetup.jwtbased.interceptors.authentication;
 
-import static io.jsonwebtoken.Jwts.parser;
 import static nl.kabisa.meetup.jwtbased.TokenNames.ACCESS_TOKEN_NAME;
 import static nl.kabisa.meetup.jwtbased.TokenNames.REFRESH_TOKEN_NAME;
 
@@ -26,51 +25,55 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
     @Value("${jwt.short_expiration_in_seconds}")
     private int expiration;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            if (handlerMethod.hasMethodAnnotation(RequireValidToken.class)) {
-                checkAccessToken(request, response);
+    private class RefreshToken {
+
+        private final String token;
+
+        private Jws<Claims> jws;
+
+        public RefreshToken(String token) {
+            this.token = token;
+            parseToken();
+        }
+
+        private void parseToken() {
+            try {
+                jws = Jwts.parser().setSigningKey(encodedKey).parseClaimsJws(token);
+            } catch (Exception e) {
             }
         }
+
+        public boolean isValid() {
+            return jws != null;
+        }
+
+        public String getSubject() {
+            return jws.getBody().getSubject();
+        }
+
+        public Date getExpiration() {
+            return jws.getBody().getExpiration();
+        }
+    }
+
+    private boolean hasValidAccessToken(HttpServletRequest request) {
+        Cookie jwtAccessCookie = WebUtils.getCookie(request, ACCESS_TOKEN_NAME);
+
+        if (jwtAccessCookie == null) {
+            return false;
+        }
+
+        String token = jwtAccessCookie.getValue();
+        try {
+            Jwts.parser().setSigningKey(encodedKey).parse(token);
+        } catch (Exception e) {
+            return false;
+        }
+
         return true;
     }
 
-    private void checkAccessToken(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        Cookie jwtAccessCookie = WebUtils.getCookie(request, ACCESS_TOKEN_NAME);
-        if (jwtAccessCookie == null) {
-            checkRefreshToken(request, response);
-        } else {
-            String token = jwtAccessCookie.getValue();
-            try {
-                parser().setSigningKey(encodedKey).parse(token);
-            } catch (Exception e) {
-                checkRefreshToken(request, response);
-            }
-        }
-    }
-
-    private void checkRefreshToken(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        Cookie jwtRefreshCookie = WebUtils.getCookie(request, REFRESH_TOKEN_NAME);
-        if (jwtRefreshCookie == null) {
-            throw new AuthenticationException();
-        }
-
-        String token = jwtRefreshCookie.getValue();
-        Jws<Claims> jws;
-        try {
-            jws = Jwts.parser().setSigningKey(encodedKey).parseClaimsJws(token);
-        } catch (Exception e) {
-            throw new AuthenticationException();
-        }
-
-        checkBlackList(jws.getBody().getSubject(), jws.getBody().getExpiration());
-
-        generateAccessToken(response, jws.getBody().getSubject());
-    }
-
-    private void generateAccessToken(HttpServletResponse response, String subject) {
+    private void generateAccessToken(String subject, HttpServletResponse response) {
         String token = Jwts.builder()
                 .setSubject(subject)
                 .setExpiration(Date.from(Instant.now().plusSeconds(expiration)))
@@ -85,9 +88,43 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
         response.addCookie(jwtAccessCookie);
     }
 
-    private void checkBlackList(String subject, Date expiration) {
-        // Throw an AuthenticationException for tokens
+    private boolean isBlacklisted(String subject, Date expiration) {
+        // Return true for tokens
         // with expiration dates and subjects that have
         // been blacklisted.
+        return false;
+    }
+
+    private void validateTokens(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (hasValidAccessToken(request)) {
+            return;
+        }
+
+        Cookie jwtRefreshCookie = WebUtils.getCookie(request, REFRESH_TOKEN_NAME);
+        if (jwtRefreshCookie == null) {
+            throw new AuthenticationException();
+        }
+
+        RefreshToken refreshToken = new RefreshToken(jwtRefreshCookie.getValue());
+        if (!refreshToken.isValid()) {
+            throw new AuthenticationException();
+        }
+
+        if (isBlacklisted(refreshToken.getSubject(), refreshToken.getExpiration())) {
+            throw new AuthenticationException();
+        }
+
+        generateAccessToken(refreshToken.getSubject(), response);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            if (handlerMethod.hasMethodAnnotation(RequireValidToken.class)) {
+                validateTokens(request, response);
+            }
+        }
+        return true;
     }
 }
