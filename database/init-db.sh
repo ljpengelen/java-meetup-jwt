1a485137268cd9dd2cab5f1ba1c5ef1df0b1b3f7
Luc Engelen <luc@kabisa.nl>
2019-08-18 08:44:47 +0200

    Create test database as well when starting container

--- database/init-db.sh
+++ database/init-db.sh
@@ -4,4 +4,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE java_meetup;
     GRANT ALL PRIVILEGES ON DATABASE java_meetup TO "$POSTGRES_USER";
+
+    CREATE DATABASE java_meetup_test;
+    GRANT ALL PRIVILEGES ON DATABASE java_meetup_test TO "$POSTGRES_USER";
 EOSQL
